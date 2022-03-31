15ea74f482131bbf0d65307b092d2a1eb1caf673
Anthony Martinez <pookins.martinez@gmail.com>
2022-03-31 15:34:29 -0600

    Simple notes added to expain project.

--- Boostability_Route_Application_Repo/Program.cs
+++ Boostability_Route_Application_Repo/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Written and Tested by Anthony R. Martinez
+//Started 3/30/2022
+//First Commited to GitHub 3/31/2022
+//This is an application that takes a list of routes and prints out the paths the application without any duplicats, throwing an error when there is a circular reference
+using System;
 
 namespace Boostability_Route_Application_Repo
 {
